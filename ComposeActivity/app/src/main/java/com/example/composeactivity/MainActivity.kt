17444c165d475b315d5b4c665a487385bd101850
JDLO <juandavidleono@gmail.com>
2023-09-18 14:36:36 +0200

    Se completa el ejercicio de tarea completada

--- ComposeActivity/app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ ComposeActivity/app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,13 +37,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("")
+                    TaskComplete()
                 }
             }
         }
     }
 }
 
+@Composable
+fun TaskComplete(modifier: Modifier = Modifier){
+    val imageSource = painterResource(R.drawable.ic_task_completed)
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        TaskImage(imageSource)
+        TaskMessageAlert()
+        TaskMessage()
+    }
+}
+
+@Composable
+fun TaskImage(image: Painter){
+    Image(painter = image, contentDescription = null)
+}
+@Composable
+fun TaskMessageAlert(){
+    Text(
+        text = "All tasks completed",
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier
+            .padding(top = 24.dp, bottom = 8.dp )
+    )
+}
+
+@Composable
+fun TaskMessage(){
+    Text(text = "Nice work!", fontSize = 16.sp)
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -50,6 +91,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeActivityTheme {
-        Greeting("")
+        TaskComplete()
     }
 }
