deca2050192aca1aa1c508ce36bd42d3f0501f5f
JDLO <juandavidleono@gmail.com>
2023-09-26 14:28:48 +0200

    Se agrega modificacion para personalizar la cantidad del porcentaje de la propina

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -55,9 +59,13 @@ fun TipTimeLayout() {
         verticalArrangement = Arrangement.Center
     ) {
         var amountInput by remember {mutableStateOf("0")}
+        var tipPercentInput by remember {mutableStateOf("0")}
+        var roundUp by remember {mutableStateOf(false)}
 
         val amount = amountInput.toDoubleOrNull() ?: 0.0
-        val tip = calculateTip(amount)
+        val tipAmount = tipPercentInput.toDoubleOrNull() ?: 0.0
+        val tip = calculateTip(amount, tipAmount, roundUp)
+        val total = calculateTotal(amount, tipAmount, roundUp)
 
         Text(
             text = stringResource(R.string.calculate_tip),
@@ -69,11 +77,29 @@ fun TipTimeLayout() {
             value = amountInput,
             onValueChange = {amountInput = it},
             modifier = Modifier
-            .padding(bottom = 32.dp)
-            .fillMaxWidth()
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
         )
+        EditNumberFieldTip(
+            value = tipPercentInput,
+            onValueChange = {tipPercentInput = it},
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
+        RoundTheTipRow(
+            roundUp,
+            {roundUp = it},
+            Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.padding(100.dp))
         Text(
             text = stringResource(R.string.tip_amount, tip),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Spacer(Modifier.padding(10.dp))
+        Text(
+            text = stringResource(R.string.total_amount, total),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -97,9 +123,76 @@ fun EditNumberField(
     )
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNumberFieldTip(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text(stringResource(R.string.tip_percentage)) },
+        keyboardOptions = KeyboardOptions( keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+    ) {
+        Text(text = stringResource(R.string.round_tip))
+        Switch(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged
+        )
+    }
+}
+@Composable
+fun DisplayTaxes(){
+    Text(
+        text = stringResource(R.string.tip_amount),
+        style = MaterialTheme.typography.displaySmall
+    )
+}
+
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean = true
+): String {
     val tip = tipPercent / 100 * amount
-    return NumberFormat.getCurrencyInstance().format(tip)
+    return if(roundUp){
+        NumberFormat.getCurrencyInstance().format(kotlin.math.ceil(tip))
+    } else {
+        NumberFormat.getCurrencyInstance().format(tip)
+    }
+}
+
+private fun calculateTotal(
+    amount: Double,
+    tipPercent: Double = 15.0,
+    roundUp: Boolean = false
+): String {
+    val total = (tipPercent / 100 * amount) + amount
+    return if(roundUp){
+        NumberFormat.getCurrencyInstance().format(kotlin.math.ceil(total))
+    } else {
+        NumberFormat.getCurrencyInstance().format(total)
+    }
 }
 
 @Preview(showBackground = true, device = "id:pixel_5")
